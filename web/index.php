21497f147e4d70baeb7a8eb19c7299435cd437c1
zevgan <me@zevgan.ru>
2018-03-05 13:14:27 +0100

    asd

--- web/index.php
+++ web/index.php
@@ -1,7 +1,8 @@
 <?php
-echo "sadas";
-/*
 
+
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
 
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
@@ -13,4 +14,4 @@ require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 $config = require __DIR__ . '/../config/web.php';
 
 (new yii\web\Application($config))->run();
-*/
+
