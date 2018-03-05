70b9a21ec28f3ce22febf4612129809c33ae0fed
zevgan <me@zevgan.ru>
2018-03-05 12:56:31 +0100

    sd

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=admin_stavba',
+    'username' => 'admin_stavba',
+    'password' => '@blacksea@',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
